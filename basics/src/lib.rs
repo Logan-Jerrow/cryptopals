c135867752b1c38ce42b6db2358e5d50369b1f18
Logan Jerrow <Logan.Jerrow@gmail.com>
2023-04-05 17:24:11 -0700

    challenge 1 & 2 done

--- basics/src/lib.rs
+++ basics/src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+Convert hex to base64
+
+The string:
+"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+Should produce:
+"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+ */
+use base64::Engine;
+pub fn hex_to_base64(hex: &str) -> String {
+    let bytes = hex::decode(hex).unwrap();
+    base64::engine::general_purpose::STANDARD.encode(bytes)
+}
+
+pub fn fixed_xor(this: &str, that: &str) -> String {
+    let this = hex::decode(this).unwrap();
+    let that = hex::decode(that).unwrap();
+    hex::encode(
+        this.iter()
+            .zip(that)
+            .map(|(t1, t2)| t1 ^ t2)
+            .collect::<Vec<_>>(),
+    )
 }
 
 #[cfg(test)]
@@ -7,8 +29,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn base64() {
+        assert_eq!(
+            hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        );
+    }
+
+    #[test]
+    fn xor() {
+        assert_eq!(
+            fixed_xor(
+                "1c0111001f010100061a024b53535009181c",
+                "686974207468652062756c6c277320657965"
+            ),
+            "746865206b696420646f6e277420706c6179"
+        );
     }
 }
